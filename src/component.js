654f237bb81fd4e82754d7b7254c677ca6d47b19
胡海星 <huhaixing@njzhyl.cn>
2023-09-26 17:27:37 +0800

    refactor: refactor

--- src/component.js
+++ src/component.js
@@ -10,7 +10,9 @@ import buildOptions from './build-options';
 
 // FIXME: Why we should define this symbol here?
 // see: https://github.com/babel/babel/blob/672b881e41228f5060bb80ea89f64315b4a1e05b/packages/babel-plugin-proposal-decorators/test/fixtures/metadata/element/exec.js
-Symbol.metadata = Symbol();
+if (Symbol.metadata === undefined) {
+  Symbol.metadata = Symbol();
+}
 
 /**
  * The decorator of Vue class components.
