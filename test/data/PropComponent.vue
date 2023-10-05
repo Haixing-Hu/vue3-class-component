9c9680d5693afe85238d440463ca264a3dc72c76
Haixing Hu <starfish.hu@gmail.com>
2023-10-05 18:19:11 +0800

    test: add some test

--- test/data/PropComponent.vue
+++ test/data/PropComponent.vue
@@ -7,18 +7,20 @@
   -
   ----------------------------------------------------------------------------->
 <template>
-  <div class="hello-component">
+  <div class="prop-component">
     <div id="message">{{ message }}</div>
     <div id="value1">{{ value1 }}</div>
     <div id="value2">{{ value2 }}</div>
     <div id="computed">{{ computedValue }}</div>
+    <div id="person-name">{{ person.name }}</div>
+    <div id="person-age">{{ person.age }}</div>
   </div>
 </template>
 <script>
 import { Component, toVue, Prop } from '../../index';
 
 @Component
-class HelloComponent {
+class PropComponent {
   @Prop
   message = 'No Message';
 
@@ -28,13 +30,20 @@ class HelloComponent {
   @Prop
   value2 = 123;
 
+  @Prop
+  person = {
+    name: 'John',
+    age: 30,
+  };
+
   mounted() {
-    console.log('HelloComponent.mounted: enter');
+    console.log('PropComponent.mounted: enter');
     console.log('this:', this);
     console.log('this.message:', this.message);
     console.log('this.value1:', this.value1);
     console.log('this.value2:', this.value2);
-    console.log('HelloComponent.mounted: exit');
+    console.log('this.person:', this.person);
+    console.log('PropComponent.mounted: exit');
   }
 
   get computedValue() {
@@ -42,5 +51,5 @@ class HelloComponent {
   }
 }
 
-export default toVue(HelloComponent);
+export default toVue(PropComponent);
 </script>
