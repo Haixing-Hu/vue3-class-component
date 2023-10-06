3a1942838d0c9afb3354224eb9fad9b3508f8dbb
Haixing Hu <starfish.hu@gmail.com>
2023-10-06 18:40:31 +0800

    test: add unit test

---
+++ test/build-options.test.js
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2022 - 2023.
+ *    Haixing Hu, Qubit Co. Ltd.
+ *
+ *    All rights reserved.
+ *
+ ******************************************************************************/
+import buildOptions from '../src/build-options';
+
+/**
+ * Unit tests the `buildOptions()` function.
+ *
+ * @author Haixing Hu
+ */
+describe('buildOptions() function', () => {
+  class MyClass {}
+  test('wrong context argument type', () => {
+    expect(() => buildOptions(MyClass, null, {}))
+      .toThrowWithMessage(TypeError, 'The context must be an object.');
+    expect(() => buildOptions(MyClass, '123', {}))
+      .toThrowWithMessage(TypeError, 'The context must be an object.');
+  });
+  test('wrong options argument type', () => {
+    expect(() => buildOptions(MyClass, { kind: 'class' }, null))
+      .toThrowWithMessage(TypeError, 'The options of the `@Component` decorator must be an object.');
+    expect(() => buildOptions(MyClass, { kind: 'class' }, 'abc'))
+      .toThrowWithMessage(TypeError, 'The options of the `@Component` decorator must be an object.');
+  });
+});
