b961c47b54f416a57377c338097b462112fd5deb
Haixing Hu <starfish.hu@gmail.com>
2023-10-16 11:20:07 +0800

    build: update the configuration of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2022 - 2023.
+ *    Haixing Hu, Qubit Co. Ltd.
+ *
+ *    All rights reserved.
+ *
+ ******************************************************************************/
 module.exports = {
   root: true,
+  parser: "@babel/eslint-parser",
   extends: [
     '@haixing_hu/eslint-config',
   ],
