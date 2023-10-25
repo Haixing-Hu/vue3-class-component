49893c1edc921ed3fb9c34b89c204db939298686
Haixing Hu <starfish.hu@gmail.com>
2023-10-25 15:24:13 +0800

    build: use @haixing_hu/rollup-builder to simplify the building script

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,52 +6,6 @@
 //    All rights reserved.
 //
 ////////////////////////////////////////////////////////////////////////////////
-import babel from '@rollup/plugin-babel';
+import rollupBuilder from '@haixing_hu/rollup-builder';
 
-function getBasicConfig(format) {
-  const config = {};
-  switch (format) {
-    case 'cjs':
-      config.format = 'cjs';
-      config.babelModules = 'cjs';
-      break;
-    case 'es':
-      config.format = 'es';
-      config.babelModules = false;
-      break;
-    default:
-      throw new Error(`Unsupported library format: ${format}`);
-  }
-  config.minify = (process.env.NODE_ENV === 'production');
-  config.filename = `vue3-class-component.${config.format}${config.minify ? '.min' : ''}.js`;
-  return config;
-}
-
-function rollupOptions(format) {
-  const config = getBasicConfig(format);
-  return {
-    external: ['vue', /@babel\/runtime/],
-    input: 'src/index.js',
-    output: {
-      name: 'Vue3ClassComponent',
-      file: `dist/${config.filename}`,
-      format: config.format,
-      compact: config.minify,
-      sourcemap: true,
-      globals: {
-        vue: 'Vue',
-      },
-    },
-    plugins: [
-      babel({
-        babelHelpers: 'runtime',
-        exclude: ['node_modules/**'],
-      }),
-    ],
-  };
-}
-
-export default [
-  rollupOptions('cjs'),
-  rollupOptions('es'),
-];
+export default rollupBuilder('VueClassComponent', import.meta.url);
